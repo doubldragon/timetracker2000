24ef78e83866ecc0f126904805a252725c57a1dd
Brandon Spencer <brandon.spencer@gmail.com>
2017-06-21 23:55:15 -0400

    Can add and delete tasks

--- app/index.php
+++ app/index.php
@@ -9,18 +9,83 @@
 <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
+<?php
+	
+	if (isset($_GET['task'])) {
+    $safeTask = htmlentities($_GET['task']);
+    addTask(getDb(), $safeTask);
+}
+	if (isset($_GET['removeTask'])) {
+    $safeTask = htmlentities($_GET['removeTask']);
+    removeTask(getDb(), $safeTask);
+  }
+	function getDb() {
+    $db = pg_connect("host=localhost port=5432 dbname=timetracking user=tracker password=track");
+    return $db;
+  }
+
+  function addTask($db, $task){
+  	$date = date('M-d-y');
+  	$time = date(' H:i:s');
+  	$stmt = 'INSERT INTO time (task, date_in,time_in) VALUES 
+  		(\''.$task.'\',\''.$date.'\',\''.$time.'\');';
+  	$result = pg_query($stmt);
+  }
+
+  function getTasks($db) {
+    $request = pg_query(getDb(), "SELECT * FROM time");
+    return pg_fetch_all($request);
+  }
+
+  function removeTask($db, $id) {
+    $stmt = "DELETE FROM time WHERE id=" . $id;
+    $result = pg_query($stmt);
+  }
+
+?>
+
+
 <div class ='container'>
 <h1>Time Tracker 2000</h1>
-
+<div>
 <form class="form-inline" method="get" action="">
 
   <label class="sr-only" for="task">Task</label>
   <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="task" name="task" placeholder="What are you working on?">
-
+  <!-- <input type='hidden' id='timePunch' name='timePunch'> -->
+<!-- <input type="date" class="form-control mb-2 mr-sm-2 mb-sm-0" id="dateOut" name="dateOut" >
+<input type="time" class="form-control mb-2 mr-sm-2 mb-sm-0" id="timeOut" name="timeOut" > -->
   
 
   <button type="submit" class="btn btn-primary">Punch the Clock</button>
 </form>
+</div>
+<?php
+  foreach (getTasks(getDb()) as $tasklist) {
+?>
+<div class="row mx-auto" style="padding: 10px 0;">
+    
+        <div class="col">
+      <div class="align-middle">
+        <?=$tasklist['task'];?>
+        
+      </div> 	
+    </div>
+    <form method="get" action="">
+      <input name="removeTask" value="<?=$tasklist['id'];?>" type="hidden">
+      <button type="submit" class="close" aria-label="Remove">
+        <span aria-hidden="true" style="padding-right: 10px;">&times;</span>
+      </button>
+    </form>
+  </div>
+  <?php 
+  }
+?>
+</div>
+
+
+
+
 
 
 </div>
