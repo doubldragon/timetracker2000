16ad927b489f3ea3ebe9d7001806cbca099acb4e
Brandon Spencer <brandon.spencer@gmail.com>
2017-06-22 10:27:03 -0400

    formatting update, can complete tasks

--- app/index.php
+++ app/index.php
@@ -19,6 +19,10 @@
     $safeTask = htmlentities($_GET['removeTask']);
     removeTask(getDb(), $safeTask);
   }
+  if (isset($_GET['completeTask'])) {
+    $safeTask = htmlentities($_GET['completeTask']);
+    completeTask(getDb(), $safeTask);
+  }
 	function getDb() {
     $db = pg_connect("host=localhost port=5432 dbname=timetracking user=tracker password=track");
     return $db;
@@ -26,14 +30,21 @@
 
   function addTask($db, $task){
   	$date = date('M-d-y');
-  	$time = date(' H:i:s');
+  	$time = date(' H:i');
   	$stmt = 'INSERT INTO time (task, date_in,time_in) VALUES 
   		(\''.$task.'\',\''.$date.'\',\''.$time.'\');';
   	$result = pg_query($stmt);
   }
 
+  function completeTask($db, $id){
+  	$date = date('M-d-y');
+  	$time = date(' H:i');
+  	$stmt = 'UPDATE time SET date_out= \''.$date.'\', time_out=\''.$time.'\' WHERE id='.$id;
+  	$result = pg_query($stmt);
+  }
+
   function getTasks($db) {
-    $request = pg_query(getDb(), "SELECT * FROM time ORDER BY date_in DESC, time_in DESC");
+    $request = pg_query(getDb(), "SELECT * FROM time WHERE date_out is null ORDER BY date_in DESC, time_in DESC");
     return pg_fetch_all($request);
   }
 
@@ -41,7 +52,7 @@
     $stmt = "DELETE FROM time WHERE id=" . $id;
     $result = pg_query($stmt);
   }
-
+$stmt = 'UPDATE time SET date_out= '.$date.'\', time_out=\''.$time.'\' WHERE id='.$id;
 ?>
 
 
@@ -51,37 +62,82 @@
 <form class="form-inline align-middle" method="get" action="">
 
   <label class="sr-only" for="task">Task</label>
+  <div class="input-group">
+    <div class="input-group-addon">Task</div>
   <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="task" name="task" placeholder="What are you working on?">
-  <!-- <input type='hidden' id='timePunch' name='timePunch'> -->
-<!-- <input type="date" class="form-control mb-2 mr-sm-2 mb-sm-0" id="dateOut" name="dateOut" >
-<input type="time" class="form-control mb-2 mr-sm-2 mb-sm-0" id="timeOut" name="timeOut" > -->
-  
-
+	</div>
   <button type="submit" class="btn btn-primary">Punch the Clock</button>
 </form>
 </div>
-<?php
-  foreach (getTasks(getDb()) as $tasklist) {
-?>
-<div class="row mx-auto" style="padding: 10px 0;">
-    
-        <div class="col taskItem">
-      <div class="align-middle">
-        <?=$tasklist['task'];?>
-        
-      </div> 	
-    </div>
+<table class='table table-responsive table-striped align-middle'>
+<tr>
+	<th>Date</th>
+	<th>Project Task</th>
+	<th>Start</th>
+	<th>Stop</th>
+	<th>Functions</th>
+</tr>
+<?php foreach (getTasks(getDb()) as $tasklist) { ?>
+
+<tbody>
+<tr>
+	<td>
+		<?=$tasklist['date_in'];?>
+	</td>
+	<td>
+		<?=$tasklist['task'];?>
+	</td>
+	<td>
+		<?=$tasklist['time_in'];?>
+	</td>
+	<td>
+		<?=$tasklist['time_out'];?>
+	</td>
+	<td>
+	<div class="btn-group" role="group">	
     <form method="get" action="">
-      <input name="removeTask" value="<?=$tasklist['id'];?>" type="hidden">
-      <button type="submit" class="close" aria-label="Remove">
-        <span aria-hidden="true" style="padding-right: 10px;">&times;</span>
+      <input name="completeTask" value="<?=$tasklist['id'];?>" type="hidden">
+      <button class='btn btn-success' type="submit button" id='completeTask' class="close" aria-label="Complete">
+        <span aria-hidden="true" >&#x2714;</span>
       </button>
     </form>
-  </div>
+    <form method="get" action="">
+      <input name="editTask" value="<?=$tasklist['id'];?>" type="hidden">
+      <button class='btn btn-warning' type="submit button" id='editTask' class="close" aria-label="Complete">
+        <span aria-hidden="true">&#x270E;</span>
+      </button>
+    </form>
+    <form method="get" action="">      
+      <input name="removeTask" value="<?=$tasklist['id'];?>" type="hidden">
+      <button class = 'btn btn-danger' type="submit " id='removeTask' class="close" aria-label="Remove">
+        <span aria-hidden="true" >&times;</span>
+      </button>
+    </form>
+    
+ 	</div>
+
+	</td>
+</tr>	
+</tbody>
   <?php 
   }
 ?>
-</div>
+
+
+</table>
+
+<!-- <div class="row mx-auto" style="padding: 10px 0;">
+    
+        <div class="col taskItem">
+      <div class="align-middle">
+        <=$tasklist['task'];?>
+        
+      </div> 	
+    </div>
+    
+    
+
+</div> -->
 
 
 
